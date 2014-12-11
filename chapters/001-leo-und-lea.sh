5d584cf27a8b9817597c67d614501b9f73406d5f
Jonas Felix <jonas.felix@felixideas.com>
2014-12-11 22:31:22 +0100

    first chaptor leo und lea

---
+++ chapters/001-leo-und-lea.sh
@@ -0,0 +1,20 @@
+agyNarrator "
+Long long time ago, in a dusty programming basement sits Leo behind his digital portal.
+Leo is hacking code for him self all day long happy and smart, as he likes it.
+One day somebody knocks on his door *knock knock*."
+
+agyLeo "Yes?"
+
+agyNarrator "
+A small female employee opens slow the door."
+
+agyLea "Hi. You'r Leo right?"
+
+agyLeo "Yes. But please create a issue and let it be checkt befor you come to me."
+
+agyNarrator "
+Leo says and turns back to his screen.
+The women laughs walks to the empty desk on the other side and drops her bag on it.
+"
+
+agyLea "I'm not here to bring issues, I'm here to kill some"
